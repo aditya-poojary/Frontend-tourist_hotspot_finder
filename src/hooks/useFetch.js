82e227679eb690aa7db830b4b804aa98115b2a18
Aditya Poojary <adityapoojary07@gmail.com>
2025-05-30 15:22:29 +0530

    Make the project production proof

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState} from "react";
+
+export default function useFetch(fetchFn, initialValue){
+    const [isFetching, setIsFetching] = useState();
+    const [error, setError] = useState();
+    const [fetchedData, setfetchedData] = useState(initialValue);
+
+
+    useEffect(() => {
+        async function fetchData() {
+          setIsFetching(true);
+          try {
+            const data = await fetchFn();
+            setfetchedData(data);
+          } catch (error) {
+            setError({ message: error.message || 'Failed to fetch data' });
+          }
+    
+          setIsFetching(false);
+        }
+    
+        fetchData();
+      }, [fetchFn]);
+
+      return { isFetching,fetchedData, error,setfetchedData};
+}
+
+
+
